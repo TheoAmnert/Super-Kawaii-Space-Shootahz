6a918744003f032a0417b8a93cd47b5843a733ab
OscarL16 <oscar.lundberg.se@gmail.com>
2022-10-27 09:11:29 +0200

    hp

--- Assets/Codes/spelare.cs
+++ Assets/Codes/spelare.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class spelare : MonoBehaviour
 {
+    #region Variablar
     [SerializeField, Range(1, 8)]
     public float speed;
     [SerializeField]
@@ -15,16 +16,36 @@ public class spelare : MonoBehaviour
     [SerializeField]
     GameObject bullet;
 
-
-    // Start is called before the first frame update
-    void Start()
+    #endregion
+    #region Health
+    private int _health = 100;
+    public int health
     {
-
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                _health = 0;
+            }
+            else if (value >= 100)
+            {
+                _health = 100;
+            }
+            else
+            {
+                _health = value;
+            }
+        }
     }
+    #endregion
 
-    // Update is called once per frame
     void Update()
     {
+        #region Movement
         if (Input.GetKey(right))
         {
             transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
@@ -36,10 +57,12 @@ public class spelare : MonoBehaviour
             transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
             print("Hejd");
         }
+        #endregion
+        #region Shooting
         if (Input.GetKeyDown(shoot))
         {
             Instantiate(bullet, transform.position, bullet.transform.rotation);
         }
-
+        #endregion
     }
 }
